8053f6204251c373603daf3cdf7b6f9ee497540c
Steven <steven.a.pinkerton@gmail.com.com>
2023-06-21 18:49:40 +0100

    Modified The trend_identifing code

--- Trend_analysis/Trend_Identification/Lstm/lstm_trend_identifier.py
+++ Trend_analysis/Trend_Identification/Lstm/lstm_trend_identifier.py
@@ -1,22 +1,22 @@
-import torch
-import torch.nn as nn
-
 class LSTM(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim, device='cpu', bidirectional=False):
         super(LSTM, self).__init__()
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
 
         # LSTM Layer
-        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True) 
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True, bidirectional=bidirectional) 
 
         # Fully connected layer
         self.fc = nn.Linear(hidden_dim, output_dim)
 
+        # Device
+        self.device = torch.device(device)
+
     def forward(self, x):
         # Initialize hidden state and cell state
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(x.device)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(self.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(self.device)
 
         # Forward propagate the LSTM
         out, _ = self.lstm(x, (h0, c0))
