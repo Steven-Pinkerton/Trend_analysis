5bb2cff1827dfda9c8a88411c5a6099fb1686182
Steven <steven.a.pinkerton@gmail.com.com>
2023-06-21 19:03:46 +0100

    Event_Identification refactored

--- Trend_analysis/Trend_Identification/Event_identification/event_identification.py
+++ Trend_analysis/Trend_Identification/Event_identification/event_identification.py
@@ -1,56 +1,49 @@
 class EventIdentifier:
     def __init__(self, sentiment_data, neg_weight=1.5):
+        """
+        sentiment_data: pandas DataFrame, expects columns 'timestamp' and 'sentiment_score'
+        """
         self.data = pd.DataFrame(sentiment_data)
         self.data['timestamp'] = pd.to_datetime(self.data['timestamp'])
         self.data.set_index('timestamp', inplace=True)
-        # Apply weighting to negative sentiment scores
         self.data['weighted_sentiment'] = np.where(self.data['sentiment_score'] < 0, self.data['sentiment_score'] * neg_weight, self.data['sentiment_score'])
 
     def group_by_time(self, freq='H'):
-        # Group by time periods and compute the mean sentiment score for each period
         self.data = self.data.resample(freq)['weighted_sentiment'].mean().reset_index()
         return self.data
 
-    def detect_high_volume_periods(self, threshold=50):
-        # Group by time periods and count the number of posts in each period
-        post_counts = self.data.resample('H').size()
-        # Identify high volume periods
+    def detect_high_volume_periods(self, freq='H', threshold=50):
+        post_counts = self.data.resample(freq).size()
         self.data['high_volume'] = post_counts > threshold
         return self.data
 
-    def detect_keyword_bursts(self, keywords, threshold=5):
-        # Compute the number of keyword occurrences in each time period
-        keyword_counts = self.data[self.data['keywords'].apply(lambda x: any(keyword in x for keyword in keywords))].resample('H').size()
-        # Identify periods with high keyword occurrences
-        self.data['keyword_burst'] = keyword_counts > threshold
+    def detect_keyword_bursts(self, keywords, freq='H', threshold=5):
+        if 'keywords' in self.data.columns:
+            keyword_counts = self.data[self.data['keywords'].apply(lambda x: any(keyword in x for keyword in keywords))].resample(freq).size()
+            self.data['keyword_burst'] = keyword_counts > threshold
         return self.data
 
     def detect_rolling_trends(self, window=3):
-        # Compute the rolling average sentiment score
         self.data['rolling_sentiment'] = self.data['sentiment_score'].rolling(window=window).mean()
         return self.data
 
     def detect_outliers(self, z_threshold=2):
-        # Compute z-scores of the sentiment scores
-        z_scores = np.abs(stats.zscore(self.data['sentiment_score']))
-        # Identify outliers
+        z_scores = np.abs(stats.zscore(self.data['weighted_sentiment']))
         self.data['is_outlier'] = z_scores > z_threshold
         return self.data
 
     def detect_change_points(self, threshold=0.05):
-        # Compute the mean sentiment score
         mean_val = np.mean(self.data['sentiment_score'])
-        # Compute the cumulative sum of the differences from the mean
         self.data['cusum'] = np.cumsum(self.data['sentiment_score'] - mean_val)
-        # Identify change points
         self.data['change_point'] = np.abs(self.data['cusum']) > threshold
         return self.data
 
     def detect_anomalies(self, contamination=0.01):
-        # Initialize the Isolation Forest
+        from sklearn.preprocessing import StandardScaler
+        scaler = StandardScaler()
+        sentiment_score_scaled = scaler.fit_transform(self.data[['sentiment_score']])
+        
         clf = IsolationForest(contamination=contamination)
-        # Fit the model and get the predictions
-        preds = clf.fit_predict(self.data[['sentiment_score']])
-        # Identify anomalies
+        preds = clf.fit_predict(sentiment_score_scaled)
         self.data['is_anomaly'] = preds == -1
         return self.data
