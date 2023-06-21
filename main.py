f331ec94cac15fa97f5f29e12a5d77908ec87589
Steven <steven.a.pinkerton@gmail.com.com>
2023-06-21 16:34:40 +0100

    Main.py created

---
+++ main.py
@@ -0,0 +1,59 @@
+from feature_extraction import FeatureExtractor
+from sentiment_analysis import SentimentAnalyzer
+from stock_data_acquisition import get_stock_data
+from stock_data_preprocessing import preprocess_stock_data
+from trend_analysis import TrendAnalyzer
+from correlation_analysis import CorrelationAnalyzer
+from data_integration import DataIntegrator
+from 4chan_scraper import monitor_board
+from dependency_parser import parse_sentence
+from fuzzy_matching import get_fuzzy_matches, is_fuzzy_match
+from keyword_generator import generate_extended_keywords
+from regex_patterns import generate_patterns, contains_keywords
+from scraper_database import create_connection, create_table, insert_thread, update_thread, delete_thread
+
+def main(url, content, api_key, board, interval):
+    # Instantiate all necessary objects
+    feature_extractor = FeatureExtractor(content, url)
+    sentiment_analyzer = SentimentAnalyzer()
+    trend_analyzer = TrendAnalyzer()
+    
+    # Extract features
+    features = feature_extractor.extract_features()
+    correlation_analyzer = CorrelationAnalyzer()
+
+    # Fetch and preprocess stock data
+    symbols = feature_extractor.extract_company(content)
+    stock_data = get_stock_data(symbols, api_key=api_key)
+    preprocessed_stock_data = preprocess_stock_data(stock_data)
+
+    # Perform sentiment analysis on the news article
+    sentiment_score = sentiment_analyzer.analyze(content)
+
+    # Extract necessary features from the news article
+    features = feature_extractor.extract(content)
+    features['sentiment_score'] = sentiment_score
+
+    # Fetch social media data from 4chan
+    monitor_board(board, interval)
+
+    # Integrate stock data with sentiment data and features
+    data_integrator = DataIntegrator(preprocessed_stock_data, features)
+    integrated_data = data_integrator.integrate_data()
+
+    # Perform trend analysis on the integrated data
+    trends = trend_analyzer.analyze(integrated_data)
+
+    # Perform correlation analysis on the integrated data
+    correlations = correlation_analyzer.analyze(integrated_data)
+
+    return trends, correlations
+
+if __name__ == "__main__":
+    url = "your_article_url"
+    content = "your_article_content"
+    api_key = "your_alpha_vantage_api_key"
+    board = "your_board"
+    interval = 60  # scrape interval in seconds
+
+    main(url, content, api_key, board, interval)
