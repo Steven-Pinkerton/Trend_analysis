71c2de19e57677cc0114056dd81fb9df7205f47d
Steven <steven.a.pinkerton@gmail.com.com>
2023-06-21 18:41:56 +0100

    Refactor of lstm model and preprocessing

--- Trend_analysis/Trend_Identification/Lstm/lstm_model.py
+++ Trend_analysis/Trend_Identification/Lstm/lstm_model.py
@@ -8,18 +8,21 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import time
 
-def create_model(sequence_length, units=50, cell=LSTM, n_layers=2, dropout=0.3,
-                 loss="mean_absolute_error", optimizer="rmsprop", bidirectional=False, output_units=1):
+def create_model(sequence_length, units_list=[50], dense_layers=1, activation='relu',
+                 cell=LSTM, dropout=0.3, loss="mean_absolute_error", 
+                 optimizer="rmsprop", bidirectional=False, output_units=1):
     model = Sequential()
 
-    for i in range(n_layers):
+    for i, units in enumerate(units_list):
         if i == 0:
             # first layer
             if bidirectional:
-                model.add(Bidirectional(cell(units, return_sequences=True), input_shape=(None, sequence_length)))
+                model.add(Bidirectional(cell(units, return_sequences=True if len(units_list) > 1 else False), 
+                                        input_shape=(None, sequence_length)))
             else:
-                model.add(cell(units, return_sequences=True, input_shape=(None, sequence_length)))
-        elif i == n_layers - 1:
+                model.add(cell(units, return_sequences=True if len(units_list) > 1 else False, 
+                               input_shape=(None, sequence_length)))
+        elif i == len(units_list) - 1:
             # last layer
             if bidirectional:
                 model.add(Bidirectional(cell(units, return_sequences=False)))
@@ -33,80 +36,46 @@ def create_model(sequence_length, units=50, cell=LSTM, n_layers=2, dropout=0.3,
                 model.add(cell(units, return_sequences=True))
         # add dropout after each layer
         model.add(Dropout(dropout))
-        
-    model.add(Dense(units=output_units))
-    
+
+    for _ in range(dense_layers):
+        model.add(Dense(units=output_units, activation=activation))
+
     model.compile(loss=loss, metrics=["mean_absolute_error"], optimizer=optimizer)
     return model
 
-def train(self, data, sequence_length=30, epochs=100):
-        """
-        Train the LSTM on the provided data.
 
-        Parameters:
-        data : array-like
-            The input data, which should be a 1D array or list of sentiment scores.
-        sequence_length : int
-            The length of the sequences to use for training the LSTM.
-        epochs : int
-            The number of epochs to train for.
-        """
-        # Convert the data to a PyTorch tensor
-        data = torch.tensor(data, dtype=torch.float)
+def train(self, model, data, sequence_length=30, epochs=100):
+    # Prepare the input and target sequences
+    X = []
+    y = []
+    for i in range(sequence_length, len(data)):
+        X.append(data[i-sequence_length:i])
+        y.append(data[i])
+    X = np.array(X)
+    y = np.array(y)
+    X = np.reshape(X, (X.shape[0], X.shape[1], 1))  # Reshape for LSTM
 
-        # Prepare the input and target sequences
-        inputs = []
-        targets = []
-        for i in range(len(data) - sequence_length):
-            inputs.append(data[i:i+sequence_length])
-            targets.append(data[i+sequence_length])
-        inputs = torch.stack(inputs)
-        targets = torch.tensor(targets, dtype=torch.float)
+    # Train the LSTM
+    history = model.fit(X, y, epochs=epochs)
 
-        # Create the optimizer
-        optimizer = optim.Adam(self.parameters())
+    return model, history
 
-        # Train the LSTM
-        for epoch in range(epochs):
-            optimizer.zero_grad()
-            outputs = self(inputs)
-            loss = torch.mean((outputs - targets) ** 2)
-            loss.backward()
-            optimizer.step()
+def predict_trends(self, model, data, sequence_length=30, num_days=30):
+    # Prepare the input sequences
+    inputs = np.array(data[-sequence_length:])
+    inputs = np.reshape(inputs, (1, sequence_length, 1))  # Reshape for LSTM
 
-def predict_trends(self, data, sequence_length=30, num_days=30):
-        """
-        Predict trends in the sentiment scores for the next N days.
+    # Predict the sentiment scores for the next N days
+    predictions = []
+    for _ in range(num_days):
+        prediction = model.predict(inputs)
+        predictions.append(prediction[0,0])
 
-        Parameters:
-        data : array-like
-            The input data, which should be a 1D array or list of sentiment scores.
-        sequence_length : int
-            The length of the sequences to use for training the LSTM.
-        num_days : int
-            The number of days to predict sentiment scores for.
+        # Update the input sequence for the next prediction
+        inputs = np.roll(inputs, -1)
+        inputs[0, -1, 0] = prediction
 
-        Returns:
-        trends : list of str
-            The predicted trends for the next N days, where each trend is either 'positive' or 'negative'.
-        """
-        # Convert the data to a PyTorch tensor
-        data = torch.tensor(data, dtype=torch.float)
+    # Identify the trends based on the predicted sentiment scores
+    trends = ['positive' if predictions[i] > predictions[i-1] else 'negative' for i in range(1, num_days)]
 
-        # Prepare the input sequences
-        inputs = [data[-sequence_length:]]
-        inputs = torch.stack(inputs)
-
-        # Predict the sentiment scores for the next N days
-        predictions = []
-        for _ in range(num_days):
-            prediction = self(inputs)
-            predictions.append(prediction.item())
-
-            # Update the input sequence for the next prediction
-            inputs = torch.cat((inputs[0, 1:], prediction)).unsqueeze(0)
-
-        # Identify the trends based on the predicted sentiment scores
-        trends = ['positive' if predictions[i] > predictions[i-1] else 'negative' for i in range(1, num_days)]
-
-        return trends
+    return trends
--- Trend_analysis/Trend_Identification/Lstm/lstm_preprocessing.py
+++ Trend_analysis/Trend_Identification/Lstm/lstm_preprocessing.py
@@ -5,21 +5,17 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 def difference(data: pd.DataFrame, column: str, interval: int = 1) -> pd.DataFrame:
-    """
-    Make a DataFrame time series stationary by differencing.
-
-    :param data: DataFrame containing the time series data
-    :param column: Name of the time series column
-    :param interval: Differencing interval, default is 1
-    :returns: DataFrame with the differenced time series
-    """
+    if column not in data.columns:
+        raise ValueError(f"Column {column} not in DataFrame.")
     data[column] = data[column].diff(interval)
     data.dropna(inplace=True)  # First row will be NaN, so drop it
     return data
 
-def plot_series(time_series):
+
+def plot_series(time_series, title='Sentiment Score'):
     plt.figure(figsize=(10,6))
     plt.plot(time_series, label='Sentiment Score')
+    plt.title(title)
     plt.legend(loc='best')
     plt.show()
     
@@ -30,28 +26,29 @@ def test_stationarity(time_series):
     for key,value in dftest[4].items():
         dfoutput['Critical Value (%s)'%key] = value
     print(dfoutput)
+    return dfoutput
     
-def normalize_series(series):
+def normalize_series(train_series, test_series=None):
     scaler = MinMaxScaler(feature_range=(-1, 1))
-    normalized_series = scaler.fit_transform(series.values.reshape(-1, 1))
-    return normalized_series, scaler
+    normalized_train_series = scaler.fit_transform(train_series.values.reshape(-1, 1))
+    normalized_test_series = scaler.transform(test_series.values.reshape(-1, 1)) if test_series is not None else None
+    return normalized_train_series, normalized_test_series, scaler
 
-def train_test_split(series, test_size=0.2):
-    split_point = int(len(series) * (1 - test_size))
-    train = series[:split_point]
-    test = series[split_point:]
-    return train, test
+def train_test_split(series, test_size=0.2, random_state=None):
+    if random_state is not None:
+        train_series, test_series = train_test_split(series, test_size=test_size, random_state=random_state)
+    else:
+        split_point = int(len(series) * (1 - test_size))
+        train_series = series[:split_point]
+        test_series = series[split_point:]
+    return train_series, test_series
+
 
 def create_sequences(data, seq_length):
-    xs = []
-    ys = []
-
-    for i in range(len(data)-seq_length-1):
-        x = data[i:(i+seq_length)]
-        y = data[i+seq_length]
-        xs.append(x)
-        ys.append(y)
-
+    data = np.array(data)
+    stride = data.strides[0]
+    sequences = stride_tricks.as_strided(data, shape=(len(data) - seq_length, seq_length), strides=(stride, stride))
+    return sequences[:-1], data[seq_length:]
     return np.array(xs), np.array(ys)
 
 def fill_missing_values(data):
