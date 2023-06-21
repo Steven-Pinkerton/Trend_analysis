4f6778f3da2f00e826efd9ea96285773895cb283
Steven <steven.a.pinkerton@gmail.com.com>
2023-06-21 19:51:17 +0100

    Improved the code within correlation_analysis changed the method from Pearson to Spearman

--- Corrlelation_Analysis/correclation_analysis.py
+++ Corrlelation_Analysis/correclation_analysis.py
@@ -1,6 +1,4 @@
-import pandas as pd
-
-def calculate_rolling_correlation(df, window_size=24, method='pearson'):
+def calculate_rolling_correlation(df, window_size=24, method='spearman', fill_method=None):
     """
     Calculate the rolling correlation between changes in sentiment and changes in stock prices.
 
@@ -11,13 +9,31 @@ def calculate_rolling_correlation(df, window_size=24, method='pearson'):
         The size of the rolling window to use for calculating correlation.
     method : str
         The correlation method to use ('pearson', 'kendall', 'spearman').
+    fill_method : str
+        The method to use for filling missing values ('bfill', 'ffill', 'interpolate').
 
     Returns:
     rolling_correlation : pandas Series
         The rolling correlation between changes in sentiment and changes in stock prices.
     """
-    # Drop any rows with missing data
-    df = df.dropna(subset=['sentiment_score', 'stock_price'])
+    # Check for the required columns
+    for column in ['sentiment_score', 'stock_price']:
+        if column not in df.columns:
+            raise ValueError(f"The DataFrame is missing the required column: {column}")
+
+    # Handle missing data
+    if fill_method is not None:
+        if fill_method == 'bfill':
+            df = df.bfill()
+        elif fill_method == 'ffill':
+            df = df.ffill()
+        elif fill_method == 'interpolate':
+            df = df.interpolate()
+        else:
+            raise ValueError("Invalid fill_method. Options are 'bfill', 'ffill', and 'interpolate'.")
+    else:
+        # Drop any rows with missing data
+        df = df.dropna(subset=['sentiment_score', 'stock_price'])
 
     rolling_correlation = df['sentiment_score'].rolling(window_size).corr(df['stock_price'], method=method)
     return rolling_correlation
