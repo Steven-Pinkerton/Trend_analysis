02fe4f487fda4807e5bcf51385238f2b8983929c
Steven <steven.a.pinkerton@gmail.com.com>
2023-06-22 02:27:28 +0100

    Changed Social_media_monitor

--- Social_Media_Monitoring/Social_Media_Monitor.py
+++ Social_Media_Monitoring/Social_Media_Monitor.py
@@ -1,20 +1,46 @@
-def monitor_social_media(sources, interval):
-    """
-    Fetch social media data from multiple sources.
+from abc import ABC, abstractmethod
+import time
 
-    Parameters:
-    sources : list
-        A list of social media sources.
-    interval : int
-        The scrape interval in seconds.
+class SocialMediaSite(ABC):
+    @abstractmethod
+    def stream_posts(self, keywords, interval):
+        pass
 
-    Returns:
-    list of pandas.DataFrame
-        A list of DataFrames, each representing data from a source.
-    """
-    social_media_data = []
-    for source in sources:
-        data = monitor_source(source, interval)  # assuming monitor_source fetches data from a source
-        social_media_data.append(data)
+
+class Twitter(SocialMediaSite):
+    def stream_posts(self, keywords, interval):
+        # Implementation for Twitter goes here.
+        pass
+
+
+class Reddit(SocialMediaSite):
+    def stream_posts(self, keywords, interval):
+        # Implementation for Reddit goes here.
+        pass
+
+
+class SocialMediaMonitor:
+    def __init__(self, sites):
+        self.sites = sites
+
+    def monitor(self, keywords, interval):
+        social_media_data = []
+        for site in self.sites:
+            data = site.stream_posts(keywords, interval)
+            social_media_data.append(data)
+        
+        return social_media_data
+
+
+if __name__ == "__main__":
+    twitter = Twitter()
+    reddit = Reddit()
+    monitor = SocialMediaMonitor([twitter, reddit])
     
-    return social_media_data
+    keywords = ["Bitcoin", "Ethereum"]  # for example
+    interval = 60  # in seconds
+
+    while True:
+        data = monitor.monitor(keywords, interval)
+        # TODO: process data
+        time.sleep(interval)
